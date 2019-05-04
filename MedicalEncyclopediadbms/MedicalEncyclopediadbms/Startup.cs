83678cb94bab118fb001013c96b8e47e8950e497
jaweria_javaid <jaweriajavaid1998@gmail.com>
2019-05-04 09:08:13 -0700

    kvk

---
+++ MedicalEncyclopediadbms/MedicalEncyclopediadbms/Startup.cs
@@ -0,0 +1,59 @@
+﻿using MedicalEncyclopediadbms.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalEncyclopediadbms.Startup))]
+namespace MedicalEncyclopediadbms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new Microsoft.AspNet.Identity.RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("A"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "A";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "ADMIN@GOOGLE.COM";
+                user.Email = "ADMIN@GOOGLE.COM";
+
+                string userPWD = "G00gle.";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "A");
+
+                }
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("U"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "U";
+                roleManager.Create(role);
+            }
+        }
+}
