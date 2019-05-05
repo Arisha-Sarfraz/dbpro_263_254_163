bd945e001c5bd1be8384f74dfa06fac4ca067f18
jaweria_javaid <jaweriajavaid1998@gmail.com>
2019-05-05 14:05:56 -0700

    ...

--- MedicalEncyclopediadbms/MedicalEncyclopediadbms/WebForm3.aspx.cs
+++ MedicalEncyclopediadbms/MedicalEncyclopediadbms/WebForm3.aspx.cs
@@ -9,12 +9,14 @@ namespace MedicalEncyclopediadbms
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
             ReportDocument Report = new ReportDocument();
             Report.Load(Server.MapPath("CrystalReport4.rpt"));
             CrystalReportViewer1.ReportSource = Report;
+
+
+
         }
     }
 }
